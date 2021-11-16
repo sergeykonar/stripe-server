52c65e203d30e41dd246118342c94174b4fe7e2a
sergeykonar <sergey.konar2002@gmail.com>
2021-11-16 12:50:58 +0100

    Procfile

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -74,7 +74,7 @@ public class Server {
     public static void main(String[] args) {
 
         Dotenv dotenv = Dotenv.load();
-        Integer i = Integer.parseInt(Objects.requireNonNull(dotenv.get("$PORT")));
+        int i = Integer.parseInt((dotenv.get("$PORT")));
         port(i);
         Stripe.apiKey = dotenv.get("STRIPE_SECRET_KEY");
 
